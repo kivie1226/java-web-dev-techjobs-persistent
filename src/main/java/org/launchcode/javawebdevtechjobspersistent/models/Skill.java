37605993c487bccceb703810fbd7d6acfb5efbb0
Kimberly Ivie <ktbt02@yahoo.com>
2020-02-02 20:45:12 -0600

    finsihed models part of part 2

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,11 +1,28 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank(message="Description required.")
     private String description;
+
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    //Getter and setter:
+    
+    public String getDescription() {
+        return description;
+    }
+    public Skill(String description) {
+        this.description = description;
+    }
+
 
     //No-arg constructor:
 
