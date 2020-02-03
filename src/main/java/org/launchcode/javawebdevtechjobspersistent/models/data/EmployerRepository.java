bfd0a09a71300b85ba99dc2cb022dcb36954494b
Kimberly Ivie <ktbt02@yahoo.com>
2020-02-02 21:26:30 -0600

    finished the employercontroller part for this step

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/data/EmployerRepository.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/data/EmployerRepository.java
@@ -4,6 +4,8 @@ import org.launchcode.javawebdevtechjobspersistent.models.Employer;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EmployerRepository extends CrudRepository<Employer, Integer> {
 }
